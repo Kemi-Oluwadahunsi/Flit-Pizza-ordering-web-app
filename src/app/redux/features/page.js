89b4edc6e1b592373b40d0614f2c297878093077
TechMrs <kemilat50@gmail.com>
2023-12-03 18:21:21 +0100

    redux fix

--- src/app/redux/features/page.js
+++ src/app/redux/features/page.js
@@ -3,10 +3,8 @@ import React from "react";
 import store from "../store/page";
 import { Provider } from "react-redux";
 
- function ReduxProvider ({children}){
+export function ReduxProvider ({children}){
     return <Provider store = {store}>
         {children}
         </Provider>
 }
-
-export default ReduxProvider
