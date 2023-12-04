fdf070c33f0881679156ef39384bc8f75f8bc8bb
TechMrs <kemilat50@gmail.com>
2023-12-04 21:08:48 +0100

    New commit

--- src/app/adminLogin/page.js
+++ src/app/adminLogin/page.js
@@ -1,21 +1,17 @@
 "use client"
 import React from 'react'
-import Link from 'next/link'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 // import axios from 'axios'
 
 const Login = () => {
 
-    const [username, setUsername] = useState(null)
-    const [password, setPassword] = useState(null)
     const [error, setError] = useState(false);
     const router = useRouter();
 
     const handleClick = async () => {
       
       try{
-        // await axios.post("http://localhost:3000/adminCookie", {username, password});
             router.push("/adminManagePage");
       }catch (err){
         setError(true)
@@ -32,14 +28,12 @@ const Login = () => {
           placeholder="username"
           type="text"
           className=" border-2 border-gray-500 h-12 text-md bg-blue-100 w-80 rounded pl-4"
-          onChange={(e) => setUsername(e.target.value)}
         ></input>
 
         <input
           placeholder="password"
           type="password"
           className="border-2 border-gray-500 h-12 text-md mt-5 bg-blue-100 rounded pl-4"
-          onChange={(e) => setPassword(e.target.value)}
         ></input>
         <button onClick={handleClick} className="mt-5 bg-teal-600 text-white text-md py-2 cursor-pointer">
           Sign In
