156c4914566d007f88e9bee9e5a60c11bf9fa74e
TechMrs <kemilat50@gmail.com>
2023-12-05 10:15:33 +0100

    New push again

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./navbar/page";
 import Footer from "./footer/page";
-import { ReduxProvider } from "./redux/features/page";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
